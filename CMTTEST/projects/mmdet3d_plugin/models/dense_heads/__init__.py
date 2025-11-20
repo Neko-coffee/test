26e2f080d27e4e364ee93cfe3f72cf85b445a507
Neko-coffee <129822131+Neko-coffee@users.noreply.github.com>
2025-11-20 20:39:24 +0900

    Delete CMTTEST directory

--- CMTTEST/projects/mmdet3d_plugin/models/dense_heads/__init__.py
+++
@@ -1,9 +0,0 @@
-from .cmt_head import (
-    SeparateTaskHead,
-    CmtHead,
-    CmtImageHead,
-    CmtLidarHead
-)
-from .cmt_aqr_head import CmtAQRHead
-
-__all__ = ['SeparateTaskHead', 'CmtHead', 'CmtLidarHead', 'CmtImageHead', 'CmtAQRHead']
--- CMTTEST/projects/mmdet3d_plugin/models/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cmt_transformer import *
-from .petr_transformer import *
-from .aqr_weight_generator import *
-from .weight_renderer import *
-from .feature_modulator import *
